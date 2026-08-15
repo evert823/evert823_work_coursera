dde60e6940689e0b524238ca89d7cfd8c1fc31d1
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2026-08-15 09:18:25 +0200

    Recap model and connect to matrices

---
+++ c3mod2assignment/makepdf.ps1
@@ -0,0 +1,1 @@
+pdflatex -output-directory="pdf" -jobname=logistic_regression_model logistic_regression_model.tex
---
+++ c3mod3assignment/makepdf.ps1
@@ -0,0 +1,1 @@
+pdflatex -output-directory="pdf" -jobname=gradient_ascent_logistic_regression gradient_ascent_logistic_regression.tex
