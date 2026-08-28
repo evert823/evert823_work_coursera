a9267fd59cd683321e97d1d31a7afdddb30770d7
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2026-08-28 17:27:43 +0200

    Start with 2nd assignment course 3 module 5

---
+++ c3mod5assignment/c3mod5assignment2.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import os
+import json
+from tree_node import TreeNode
+
+def print_with_tms(message):
+    mytimestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{mytimestamp}|{message}")
+
+def read_data(path=".\\", file_name="data.csv", dtype_dict=None):
+    mydata = pd.read_csv(os.path.join(path, file_name), dtype=dtype_dict)
+    return mydata
+
+def lending_club_dtype_dict():
+    string_columns = [
+        "term", "grade", "sub_grade", "emp_title", "emp_length",
+        "home_ownership", "is_inc_v", "issue_d", "loan_status",
+        "pymnt_plan", "url", "desc", "purpose", "title", "zip_code",
+        "addr_state", "earliest_cr_line", "initial_list_status",
+        "last_pymnt_d", "next_pymnt_d", "last_credit_pull_d",
+        "final_d", "status"
+    ]
+
+    integer_columns = [
+        "id", "member_id", "loan_amnt", "funded_amnt", "policy_code",
+        "not_compliant", "inactive_loans", "bad_loans", "emp_length_num",
+        "delinq_2yrs_zero", "pub_rec_zero", "collections_12_mths_zero",
+        "short_emp", "last_delinq_none", "last_record_none",
+        "last_major_derog_none"
+    ]
+
+    float_columns = [
+        "funded_amnt_inv", "int_rate", "installment", "annual_inc",
+        "dti", "delinq_2yrs", "inq_last_6mths", "mths_since_last_delinq",
+        "mths_since_last_record", "open_acc", "pub_rec", "revol_bal",
+        "revol_util", "total_acc", "out_prncp", "out_prncp_inv",
+        "total_pymnt", "total_pymnt_inv", "total_rec_prncp",
+        "total_rec_int", "total_rec_late_fee", "recoveries",
+        "collection_recovery_fee", "last_pymnt_amnt",
+        "collections_12_mths_ex_med", "mths_since_last_major_derog",
+        "grade_num", "sub_grade_num", "payment_inc_ratio"
+    ]
+
+    return {
+        **{column: "string" for column in string_columns},
+        **{column: "Int64" for column in integer_columns},
+        **{column: "float64" for column in float_columns},
+    }
+
+def assess_dataframe(df):
+    print_with_tms(f"rowcount {df.shape[0]} colcount {df.shape[1]}")
+    print_with_tms(f"dtypes {dict(df.dtypes)}")
+    print_with_tms(f"columns\n{df.columns.tolist()}")
+    if PRINTSTUFF == True:
+        print_with_tms("First 5 rows:")
+        print_with_tms(df.head())
+
+def define_x_features():
+    features = ['grade',                     # grade of the loan
+                'term',                      # the term of the loan
+                'home_ownership',            # home_ownership status: own, mortgage or rent
+                'emp_length',                # number of years of employment (categorical format)
+            ]
+    return features
+
+def prepare_data(df, x_features, y_features):
+    df2 = df.copy()
+    df2['safe_loans'] = df2['bad_loans'].apply(lambda x : +1 if x==0 else -1)
+    df2 = df2.drop(columns=["bad_loans"])
+    return df2[x_features + y_features]
+
+def apply_one_hot_encoding(df):
+    df2 = pd.get_dummies(df, dtype=int)
+    return df2
+
+PRINTSTUFF = False
+
+print_with_tms("Start script")
+path = os.path.join("C:\\", "Users", "Evert Jan", "courseradatascience",
+                       "course03", "module05", "data")
+
+file_name_inp = "lending-club-data.csv"
+all_data_df = read_data(path=path, file_name=file_name_inp, dtype_dict=lending_club_dtype_dict())
+
+x_features = define_x_features()
+y_features = ['safe_loans']
+
+all_data_df = prepare_data(df=all_data_df, x_features=x_features, y_features=y_features)
+all_data_df = apply_one_hot_encoding(df=all_data_df)
+x_features_enc = [cn for cn in all_data_df.columns.to_list() if cn not in y_features]
+assess_dataframe(all_data_df)
+
+#Use index files for train and test split (to reproduce a particular split)
+with open(os.path.join(path, "module-5-assignment-2-train-idx.json")) as f:
+    train_idx = json.load(f)
+train_data_df = all_data_df.iloc[train_idx]
+with open(os.path.join(path, "module-5-assignment-2-test-idx.json")) as f:
+    test_idx = json.load(f)
+test_data_df = all_data_df.iloc[test_idx]
+if PRINTSTUFF == True:
+    print_with_tms(f"total {len(all_data_df)} train {len(train_data_df)} test {len(test_data_df)}")
---
+++ c3mod5assignment/tree_node.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class TreeNode:
+    def __init__(self):
+        self.npreference = np.array([])
