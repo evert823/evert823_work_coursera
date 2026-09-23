dbf6e33fe8d7a2cb97b5722163b29b3b9d7628a7
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2026-09-23 20:12:47 +0200

    course 4 module 2 assignment - read data and sparese matrices

---
+++ c4mod2assignment/c4mod2assignment.py
@@ -0,0 +1,96 @@
+import os
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import json
+from scipy import sparse
+
+def print_with_tms(message):
+    mytimestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{mytimestamp}|{message}")
+
+def people_wiki_dtype_dict():
+    dtype_dict = {'URI':str, 'name':str, 'text':str}
+    return dtype_dict
+
+def read_data(path=".\\", file_name="data.csv", dtype_dict=None):
+    mydata = pd.read_csv(
+        os.path.join(path, file_name),
+        sep=",",
+        quotechar='"',
+        dtype=dtype_dict
+    )
+    return mydata
+
+def assess_dataframe(df):
+    print_with_tms(f"rowcount {df.shape[0]} colcount {df.shape[1]}")
+    print_with_tms(f"dtypes {dict(df.dtypes)}")
+    print_with_tms(f"columns\n{df.columns.tolist()}")
+    if PRINTSTUFF == True:
+        print_with_tms("First 5 rows:")
+        print_with_tms(df.head())
+
+def read_word_map(path, file_name="word_map.json"):
+    with open(
+        os.path.join(path, file_name),
+        mode="r",
+        encoding="utf-8"
+    ) as file:
+        return json.load(file)
+
+def read_sparse_npz(path, file_name="some_sparse_matrix.npz"):
+    file_path = os.path.join(path, file_name)
+
+    with np.load(file_path, allow_pickle=False) as archive:
+        return sparse.csr_matrix(
+            (
+                archive["data"],
+                archive["indices"],
+                archive["indptr"]
+            ),
+            shape=tuple(archive["shape"])
+        )
+
+def assess_sparse_matrix(word_map, sparse_matrix):
+    word_map_items = list(word_map.items())
+    print(f"number of words in word_map {len(word_map_items)} {word_map_items[:2]} ... {word_map_items[-2:]}")
+    print_with_tms(f"type(word_map) {type(word_map)}")
+    print_with_tms(f"type(sparse_matrix) {type(sparse_matrix)}")
+    print_with_tms(f"sparse_matrix.shape {sparse_matrix.shape}")
+    print_with_tms(f"sparse_matrix.nnz {sparse_matrix.nnz}")
+    print_with_tms(f"sparse_matrix.format {sparse_matrix.format}")
+    print_with_tms(f"sparse_matrix.dtype {sparse_matrix.dtype}")
+    index_to_word = {index: word for word, index in word_map.items()}
+    first_row = sparse_matrix.getrow(0)
+    if PRINTSTUFF == True:
+        for word_index, count in zip(first_row.indices, first_row.data):
+            print(index_to_word[word_index], count)
+
+
+PRINTSTUFF = False
+
+print_with_tms("script started")
+
+path = os.path.join("C:\\", "Users", "Evert Jan", "courseradatascience",
+                       "course04", "module02", "data")
+
+'''
+The input file is comma separated and with text qualifier double quotes
+'''
+file_name_inp = "people_wiki.csv"
+file_name_word_map = "people_wiki_map_index_to_word.json"
+file_name_word_count = "people_wiki_word_count.npz"
+file_name_tf_idf = "people_wiki_tf_idf.npz"
+
+all_data_df = read_data(path=path, file_name=file_name_inp, dtype_dict=people_wiki_dtype_dict())
+assess_dataframe(df=all_data_df)
+
+word_map = read_word_map(path=path, file_name=file_name_word_map)
+
+word_count = read_sparse_npz(path=path, file_name=file_name_word_count)
+assess_sparse_matrix(word_map=word_map, sparse_matrix=word_count)
+#For this we should have used sklearn.Countvectorizer but we need assessment compatibility
+
+tf_idf = read_sparse_npz(path=path, file_name=file_name_tf_idf)
+assess_sparse_matrix(word_map=word_map, sparse_matrix=tf_idf)
+#For this we should have used TfidfVectorizer but we need assessment compatibility
